8f567095768e6cfe82b7b0f18e91be9dfaaf3e0a
maurilyo <maurilyo@gmail.com>
2026-07-30 16:29:02 -0300

    meu app fucionando

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from workshop-docker-live!")
+import streamlit as st
 
+def hello_world():
+    return "Hello, World!"
+
+def main():
+    st.write(hello_world())
 
 if __name__ == "__main__":
     main()
